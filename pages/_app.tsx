e280dd38979a0fe843c07f24240b40c2082a8f37
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-19 21:33:42 +0100

    feat: added sass and global container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { UserContextProvider } from "../context/UserContext";
 
+import "../styles/global.scss";
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <UserContextProvider>
