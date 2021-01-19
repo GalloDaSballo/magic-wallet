30b6262772dd8d1472529ffc61be098e5f17d6a2
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-19 21:35:58 +0100

    feat: wallet styles

--- components/Wallet/index.tsx
+++ components/Wallet/index.tsx
@@ -6,6 +6,9 @@ import { formatETH } from "../../utils/format";
 
 import Send from "../Send";
 import Receive from "../Receive";
+import CopyUserAddress from "../CopyUserAddress/CopyUserAddress";
+
+import styles from "./Wallet.module.scss";
 
 const Wallet = (): JSX.Element | null => {
     const [send, setSend] = useState(false);
@@ -24,33 +27,59 @@ const Wallet = (): JSX.Element | null => {
     if (!user) {
         return null;
     }
+
+    if (send) {
+        return <Send goBackToWallet={() => setSend(false)} />;
+    }
+
+    if (receive) {
+        return <Receive goBackToWallet={() => setReceive(false)} />;
+    }
+
     return (
-        <div>
-            <p>
-                ETH: {formatETH(ethBalance)}{" "}
-                <button onClick={reloader}>Reload</button>
-            </p>
-            <div>
-                {balances.map((token) => (
-                    <div key={token.symbol}>
-                        {token.balance.gt(0) && (
-                            <p>
-                                {token.symbol} {token.balance.toString()}
-                            </p>
-                        )}
-                    </div>
-                ))}
+        <section className={styles.wallet}>
+            <div className={styles.wallet__balance}>
+                <span>Your Balance</span>
+                <p className={styles.wallet__totalBalance}>
+                    {formatETH(ethBalance)} ETH
+                </p>
+
+                <span>Your Address</span>
+                <CopyUserAddress address={user.address} color="blue" />
             </div>
-            <p>{user.address}</p>
 
-            {send && <Send />}
-            {receive && <Receive />}
+            <div className={styles.main}>
+                <button onClick={reloader} className={styles.main__reloadBtn}>
+                    Reload Quantities
+                </button>
+                <div className={styles.main__quantity}>
+                    {balances.map((token) => (
+                        <div key={token.symbol}>
+                            {token.balance.gt(0) && (
+                                <p>
+                                    {token.symbol} {token.balance.toString()}
+                                </p>
+                            )}
+                        </div>
+                    ))}
 
-            <button onClick={() => setSend(true)}>Send</button>
-            <button onClick={() => setReceive(true)}>Receive</button>
+                    {/* 
+                        TODO
+                    <div className={styles.quantity__item}>
+                        <img src="/images/etherium-small.svg" alt="Etherium" />
+                        <div>
+                            <span>150 DAI</span> <small>$150 USD</small>
+                        </div>
+                    </div> */}
+                </div>
 
-            <button onClick={logout}>Logout</button>
-        </div>
+                {/* <button onClick={logout}>Logout</button> */}
+            </div>
+            <div className={styles.wallet__buttons}>
+                <button onClick={() => setSend(true)}>Send</button>
+                <button onClick={() => setReceive(true)}>Receive</button>
+            </div>
+        </section>
     );
 };
 
