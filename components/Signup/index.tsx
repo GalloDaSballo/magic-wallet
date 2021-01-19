355a9e28c17eacfe4043c069374f85e29f447283
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-19 21:34:47 +0100

    feat: signup styles

--- components/Signup/index.tsx
+++ components/Signup/index.tsx
@@ -1,6 +1,8 @@
 import { useState, FormEvent } from "react";
 import { useLogin } from "../../context/UserContext";
 
+import styles from "./Signup.module.scss";
+
 const Signup = (): JSX.Element => {
     const [email, setEmail] = useState("");
 
@@ -12,13 +14,21 @@ const Signup = (): JSX.Element => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-        </form>
+        <section className={styles.signup}>
+            <h1>Magic Wallet</h1>
+            <img src="images/main-image.svg" alt="Magic Wallet" />
+            <span>Log in to your wallet</span>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="hello@example.com"
+                />
+
+                <button type="submit">Log in</button>
+            </form>
+        </section>
     );
 };
 
