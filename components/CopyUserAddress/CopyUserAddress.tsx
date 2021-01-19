ca18775848c36424fba300de1a75a1e360e11005
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-01-19 21:35:19 +0100

    feat: copy user address component

---
+++ components/CopyUserAddress/CopyUserAddress.tsx
@@ -0,0 +1,44 @@
+import styles from "./CopyUserAddress.module.scss";
+
+type CopyUserAddressProps = {
+    address: string;
+    color: "blue" | "gray";
+};
+
+const CopyUserAddress = ({
+    address,
+    color,
+}: CopyUserAddressProps): JSX.Element => {
+    const shortenUserAddress = (): string => {
+        return `${address.slice(0, 6)}...${address.slice(-4)}`;
+    };
+
+    const copyUserAddress = (): void => {
+        navigator.clipboard.writeText(address);
+    };
+
+    return (
+        <div
+            className={`${styles.copyUserAddress} ${
+                color === "blue"
+                    ? styles.copyUserAddress__blue
+                    : styles.copyUserAddress__gray
+            }`}
+        >
+            <button
+                title="Copy Address"
+                onClick={() => copyUserAddress(address)}
+            >
+                <img
+                    src={`/images/copy-${
+                        color === "blue" ? "blue" : "gray"
+                    }.svg`}
+                    alt="Copy"
+                />
+            </button>
+            <p>{shortenUserAddress(address)}</p>
+        </div>
+    );
+};
+
+export default CopyUserAddress;
